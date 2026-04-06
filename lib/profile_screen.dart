c70e74de91308d678d4c47faf8bff236bfcd9ce6
Md. Akash Ahmed <mdakash30453@gmail.com>
2026-04-06 12:55:46 +0600

    Update

---
+++ lib/profile_screen.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade200,
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Center(
+            child: Column(
+              children: [
+                // profile section ======================================
+                Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        shape: BoxShape.rectangle,
+                        color: Colors.white,
+                        border: BoxBorder.all(
+                          color: Colors.black87,
+                          width: 0.2,
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 10),
+                          // User Image
+                          CircleAvatar(
+                            radius: 70,
+                            backgroundImage: NetworkImage(
+                              'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg',
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          // User Name
+                          Text(
+                            'John Doe',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black87,
+                            ),
+                          ),
+
+                          // User Profession
+                          Text(
+                            'Flutter Developer',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black.withOpacity(0.5),
+                              letterSpacing: 0,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          // Person Bio
+                          Text(
+                            'Passionate about crating user-friendly and engaing digital experiences.',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black.withOpacity(0.7),
+                              letterSpacing: 0,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 5),
+                          Divider(thickness: 1, indent: 20, endIndent: 20),
+                          // Person contact information
+                          // email
+                          ListTile(
+                            visualDensity: VisualDensity(
+                              horizontal: 0,
+                              vertical: -4,
+                            ),
+                            leading: Icon(Icons.email, size: 25),
+                            title: Text('john.doe@example.com'),
+                            titleTextStyle: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                          //  number
+                          ListTile(
+                            visualDensity: VisualDensity(
+                              horizontal: 0,
+                              vertical: -4,
+                            ),
+                            leading: Icon(Icons.phone, size: 25),
+                            title: Text('+123 456 789'),
+                            titleTextStyle: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                              letterSpacing: 0,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          // User Follow & Message
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                height: 50,
+                                width: 140,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color(0XFF0000FF),
+                                    foregroundColor: Colors.white,
+                                    textStyle: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text('Follow'),
+                                ),
+                              ),
+                              SizedBox(width: 10),
+
+                              SizedBox(
+                                height: 50,
+                                width: 140,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    foregroundColor: Colors.black,
+                                    textStyle: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      side: BorderSide(
+                                        color: Colors.black.withOpacity(0.1),
+                                        width: 1,
+                                      ),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text('Message'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 14),
+                // Divider
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(width: 180, child: Divider(thickness: 2)),
+                    SizedBox(width: 9),
+                    Text(
+                      'Interests',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 9),
+                    SizedBox(width: 180, child: Divider(thickness: 2)),
+                  ],
+                ),
+
+                // Card Section
+                Container(
+                  height: 200,
+                  width: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.network(
+                        'https://live.staticflickr.com/1482/24161322546_4b26a2fa98_h.jpg',
+                        height: 100,
+                        width: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          'Travel',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          'Exploring new place around the word',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey.shade300,
+                          ),
+                          onPressed: () {},
+                          child: Text('View More'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
